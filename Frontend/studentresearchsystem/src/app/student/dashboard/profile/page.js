e4bad26167a550f6a3a9cab955f15c4492741fad
yugal687 <pageniyugal@gmail.com>
2024-10-20 21:40:29 -0500

    front initials

--- Frontend/studentresearchsystem/src/app/student/dashboard/profile/page.js
+++ Frontend/studentresearchsystem/src/app/student/dashboard/profile/page.js
@@ -1,15 +1,10 @@
 import React from "react";
 import Dashboard from "@/components/DashboardLayout";
 
-const page = () => {
+const profile = () => {
   return (
-    <div>
-      <Dashboard>
-        <div className="pt-3 pb-2 mb-3 border-bottom">
-          <h2>Student Dashboard</h2>
-        </div>
-
-        {/* Profile Information */}
+    <Dashboard>
+      <div>
         <section className="mb-4">
           <h4>Profile Information</h4>
           <div className="card p-3 mb-4">
@@ -39,32 +34,9 @@ const page = () => {
             </div>
           </div>
         </section>
-
-        {/* Resume Upload */}
-        <section className="mb-4">
-          <h4>Upload Your Resume</h4>
-          <div className="card p-3">
-            <form>
-              <div className="mb-3">
-                <label htmlFor="resumeUpload" className="form-label">
-                  Select resume file (PDF or DOCX):
-                </label>
-                <input
-                  className="form-control"
-                  type="file"
-                  id="resumeUpload"
-                  accept=".pdf,.doc,.docx"
-                />
-              </div>
-              <button type="submit" className="btn btn-primary">
-                Upload Resume
-              </button>
-            </form>
-          </div>
-        </section>
-      </Dashboard>
-    </div>
+      </div>
+    </Dashboard>
   );
 };
 
-export default page;
+export default profile;
